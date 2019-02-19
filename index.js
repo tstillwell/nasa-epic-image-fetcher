457a572511c39e68fa98babc12c8e2acec02b0db
tstillwell <tristanstillwell@gmail.com>
2019-02-18 20:08:17 -0700

    feat: Add year parsing from date var in retrieveImagesFromJSON

--- index.js
+++ index.js
@@ -23,5 +23,6 @@ const retrieveImagesFromJSON = (json) => {
   const baseUrl = 'https://epic.gsfc.nasa.gov/archive/natural';
   for (var i = 0; i < imageCount; i = i + 1){
     let date = json[i].date.substring(0, 10);
+    let year = date.substring(0, 4);
   }
 };
