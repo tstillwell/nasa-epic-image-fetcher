ee975ded3620ca9d6c47aa98f11352954c48c839
tstillwell <tristanstillwell@gmail.com>
2019-02-20 14:25:10 -0700

    feat: Add month string to imagePath

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const retrieveImagesFromJSON = (json) => {
     let month = date.substring(5, 7);
     let day = date.substring(8, 10);
     let imageName = json[i].image;
-    let imagePath = "/" + year + "/";
+    let imagePath = "/" + year + "/" + month;
     console.log(imagePath);
   }
 };
