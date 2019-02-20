bf64d98451fad8aca605bef56acf989511d10610
tstillwell <tristanstillwell@gmail.com>
2019-02-20 14:29:31 -0700

    feat: Add imageName to imagePath

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const retrieveImagesFromJSON = (json) => {
     let month = date.substring(5, 7);
     let day = date.substring(8, 10);
     let imageName = json[i].image;
-    let imagePath = "/" + year + "/" + month + "/" + day + "/";
+    let imagePath = "/" + year + "/" + month + "/" + day + "/"+ imageName;
     console.log(imagePath);
   }
 };
